d20b9f253c36a43db7b52ae32a5bc601d3d063df
Dan-Sette <daniel.sette@hotmail>
2021-06-28 14:35:18 -0300

    Test bônus

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dalves-s <dalves-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 17:28:13 by dalves-s          #+#    #+#             */
-/*   Updated: 2021/06/28 13:36:24 by dalves-s         ###   ########.fr       */
+/*   Updated: 2021/06/28 14:32:56 by dalves-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,7 +77,7 @@ char	split_line(int fd, char **line_buf, char **buf, int *bytes)
 
 int	get_next_line(int fd, char **line)
 {
-	static char	*line_buf;
+	static char	*line_buf[RLIMIT_NOFILE];
 	char		*buf;
 	int			bytes;
 	int			check;
@@ -85,15 +85,15 @@ int	get_next_line(int fd, char **line)
 	bytes = BUFFER_SIZE;
 	if ((fd < 0) || !line || (BUFFER_SIZE <= 0) || fd > RLIMIT_NOFILE)
 		return (-1);
-	if (!line_buf)
-		line_buf = ft_strdup("");
+	if (!line_buf[fd])
+		line_buf[fd] = ft_strdup("");
 	buf = malloc(sizeof(char) * BUFFER_SIZE + 1);
 	if (!buf)
 		return (-1);
-	check = split_line(fd, &line_buf, &buf, &bytes);
+	check = split_line(fd, &line_buf[fd], &buf, &bytes);
 	if (!check)
 		return (-1);
-	line_buf = new_line(line, &line_buf, &bytes);
+	line_buf[fd] = new_line(line, &line_buf[fd], &bytes);
 	if (!bytes)
 		return (0);
 	return (1);
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dalves-s <dalves-s@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/16 17:28:13 by dalves-s          #+#    #+#             */
+/*   Updated: 2021/06/26 01:02:28 by dalves-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*joined;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	joined = (char *) malloc((ft_strlen(s1) + ft_strlen(s2)) + 1);
+	if (joined == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		joined[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		joined[i] = s2[j];
+		j++;
+		i++;
+	}
+	joined[i] = '\0';
+	return (joined);
+}
+
+size_t	ft_strlcpy(char *dst, char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	if ((src == 0) || (dst == 0))
+		return (0);
+	if (size > 0)
+	{
+		while (i < (size - 1) && src[i] != '\0')
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (ft_strlen(src));
+}
+
+char	*ft_substr(char *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	size_t	size_s;
+	size_t	max_len;
+
+	size_s = ft_strlen(s);
+	if (s == NULL)
+		return (NULL);
+	if (size_s < start)
+	{
+		sub = (char *)malloc(sizeof(char) * 1);
+		if (!sub)
+			return (NULL);
+		return (sub);
+	}
+	max_len = size_s - start;
+	if (len > max_len)
+		len = max_len;
+	sub = (char *)malloc(sizeof(char) * len + 1);
+	if (!sub)
+		return (NULL);
+	ft_strlcpy(sub, &s[start], len + 1);
+	return (sub);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dst;
+	size_t	len;
+	size_t	i;
+
+	i = 0;
+	len = ft_strlen(src);
+	dst = (char *) malloc((sizeof(char) * len) + 1);
+	if (dst == NULL)
+		return (NULL);
+	while (i < len)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
